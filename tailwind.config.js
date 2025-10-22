f24b160ee044462f0ad648d8c18eb189779af725
Originsih <3542370660@example.com>
2025-10-22 21:47:29 +0800

    1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.astro',
+    './src/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
